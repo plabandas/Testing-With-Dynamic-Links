cf3a7f0eacfb3573b6a5304276a368051ad4722c
plabandas <turjoyplaban09@gmail.com>
2022-03-01 10:41:33 +0600

    Dynamic Links with DeepLinking

---
+++ app/src/main/java/com/dynamicsoftco/dynamiclinks/MainActivity.java
@@ -0,0 +1,93 @@
+package com.dynamicsoftco.dynamiclinks;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView1, textView2;
+
+    //Deeplinking URL Chilo = https://www.example.com/?email=https://www.kalerkantho.com/&password=12345
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView1 = findViewById(R.id.tv1);
+        textView2 = findViewById(R.id.tv2);
+
+        getdynamiclinkfromfirebase(); //this is for deeplink
+        //appLinkIntregation();  //This is for AppLink
+    }
+
+    //This is for deepLink
+    private void getdynamiclinkfromfirebase() {
+
+        FirebaseDynamicLinks.getInstance()
+                .getDynamicLink(getIntent())
+                .addOnSuccessListener(this, new OnSuccessListener<PendingDynamicLinkData>() {
+                    @Override
+                    public void onSuccess(PendingDynamicLinkData pendingDynamicLinkData) {
+
+                        Log.i(TAG, "We Have Dynamic Link");
+
+                        // Get deep link from result (may be null if no link is found)
+                        Uri deepLink = null;
+                        if (pendingDynamicLinkData != null) {
+                            deepLink = pendingDynamicLinkData.getLink();
+                        }
+
+                        if(deepLink!=null){
+                            Log.i(TAG, "onSuccess: We Have Dynamic Link \n" + deepLink.toString());
+
+                            String email= deepLink.getQueryParameter("email");
+                            String password = deepLink.getQueryParameter("password");
+
+                            textView1.setText(email);
+                            textView2.setText(password);
+                        }
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        Log.w(TAG, "getDynamicLink:onFailure", e);
+
+                        Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    //This is for AppLink
+    private void appLinkIntregation() {
+        // ATTENTION: This was auto-generated to handle app links.
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
+
+        if(appLinkData!= null)
+        {
+            String a = appLinkData.toString();
+            textView1.setText(a);
+            Toast.makeText(MainActivity.this, "Link"+a, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
